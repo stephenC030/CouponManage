b75086ee9312de161caf89be802249878b7ab051
mrronjt <ronjtchen@gmail.com>
2020-07-29 09:16:12 -0700

    Completed Eureka Config

---
+++ coupon-eureka/src/main/java/com/imooc/coupon/filter/TokenFilter.java
@@ -0,0 +1,28 @@
+package com.imooc.coupon.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+@Slf4j
+public class TokenFilter extends AbstractPreZuulFilter {
+    @Override
+    protected Object cRun() {
+        HttpServletRequest request = context.getRequest();
+        log.info(String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString()));
+        Object token = request.getParameter("token");
+        if(null == token){
+            String msg = "error: token is empty";
+            log.error(msg);
+            return fail(401, msg);
+        }
+        return success();
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+}
