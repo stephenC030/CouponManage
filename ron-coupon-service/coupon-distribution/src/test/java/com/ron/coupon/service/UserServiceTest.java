758686633d4cd20f178b9a101f8fe0d2f526e7a2
mrronjt <ronjtchen@gmail.com>
2020-08-18 11:45:09 -0700

    Fixes #28

---
+++ ron-coupon-service/coupon-distribution/src/test/java/com/ron/coupon/service/UserServiceTest.java
@@ -0,0 +1,46 @@
+package com.ron.coupon.service;
+
+import com.alibaba.fastjson.JSON;
+import com.ron.coupon.constant.CouponStatus;
+import com.ron.coupon.exception.CouponException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/** Test cases for User Service */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class UserServiceTest {
+
+    private Long fakeUserId = 20001L;
+    @Autowired
+    private IUserService userService;
+
+    @Test
+    public void testFindCouponsByStatus() throws CouponException{
+        System.out.println(JSON.toJSONString(
+                userService.findCouponsByStatus(
+                        fakeUserId, CouponStatus.USABLE.getCode()
+                )
+        ));
+        System.out.println(JSON.toJSONString(
+                userService.findCouponsByStatus(
+                        fakeUserId, CouponStatus.EXPIRED.getCode()
+                )
+        ));
+        System.out.println(JSON.toJSONString(
+                userService.findCouponsByStatus(
+                        fakeUserId, CouponStatus.USED.getCode()
+                )
+        ));
+    }
+
+    @Test
+    public void testFindAvailableTemplate() throws CouponException{
+        System.out.println(JSON.toJSONString(
+                userService.findAvailableTemplate(fakeUserId)
+        ));
+    }
+}
